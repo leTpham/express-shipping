73fabda34476e9c14035e81420f16751425cdc57
Le Pham <phamtle724@gmail.com>
2022-08-23 16:32:43 -0700

    wrote schema for shipment, and test for invalid API input

--- routes/shipments.js
+++ routes/shipments.js
@@ -2,6 +2,10 @@
 
 const express = require("express");
 const router = new express.Router();
+const { BadRequestError } = require("../expressError")
+
+const jsonschema = require("jsonschema");
+const shipSchema = require("../schemas/shipSchema.json");
 
 const { shipProduct } = require("../shipItApi");
 
@@ -15,6 +19,14 @@ const { shipProduct } = require("../shipItApi");
 
 router.post("/", async function (req, res, next) {
   const { productId, name, addr, zip } = req.body;
+  const result = jsonschema.validate(
+    req.body, shipSchema, { required: true });
+
+  if (!result.valid) {
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
 });
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -15,4 +15,23 @@ describe("POST /", function () {
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
+
+  test("invalid", async function () {
+    const resp = await  request(app).post("/shipments").send({
+      productId: 1,
+      name: 34,
+      addr: true,
+      zip: 9009
+    });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error.message).toEqual(
+      [
+            "instance.productId must be greater than or equal to 1000",
+            "instance.name is not of a type(s) string",
+            "instance.addr is not of a type(s) string",
+            "instance.zip is not of a type(s) string"
+      ])
+  });
+
+
 });
